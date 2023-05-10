48eb635d48e1517c4a69802d5d831de87b3b88a2
محمدحسین فخرآوری <fakhravary@gmail.com>
2023-05-10 15:22:32 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/actionfloat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.actionfloat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'KindaCode.com',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _buttonAnimatedIcon;
+  late Animation<double> _translateButton;
+  bool _isExpanded = false;
+
+  @override
+  initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600))
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _buttonAnimatedIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _translateButton = Tween<double>(
+      begin: 100,
+      end: -20,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  _toggle() {
+    if (_isExpanded) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+
+    _isExpanded = !_isExpanded;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Fakhravari.Ir')),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Transform(
+            transform: Matrix4.translationValues(
+              0.0,
+              _translateButton.value * 4,
+              0.0,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.blue,
+              onPressed: () {},
+              child: const Icon(
+                Icons.email,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4.translationValues(
+              0,
+              _translateButton.value * 3,
+              0,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              onPressed: () {},
+              child: const Icon(
+                Icons.call,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4.translationValues(
+              0,
+              _translateButton.value * 2,
+              0,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.amber,
+              onPressed: () {},
+              child: const Icon(Icons.message),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: _toggle,
+            child: AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: _buttonAnimatedIcon,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
